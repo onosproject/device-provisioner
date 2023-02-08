da255dabbcb34e54d4ed07581d3a6707936e508e
adibrastegarnia <adib.rastegarnia@intel.com>
2023-02-07 17:16:00 -0800

    simplify the device provisioner controller

--- pkg/store/config/config_store.go
+++ pkg/store/config/config_store.go
@@ -2,8 +2,8 @@
 //
 // SPDX-License-Identifier: Apache-2.0
 
-// Package store contains the stores for tracking pipeline and chassis configurations and their deployments
-package store
+// Package config contains the stores for tracking pipeline and chassis configurations and their deployments
+package config
 
 import (
 	"context"
